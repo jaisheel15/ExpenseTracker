5857f4e3b8eb71d2179fdbb74e665a0b252534c1
jaisheel15 <jaisheel021@gmail.com>
2024-10-22 18:37:41 +0530

    second commit

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import app from './app'
-Bun.serve({
+const server = Bun.serve({
+    port:process.env.PORT||3000,
     fetch: app.fetch
 })
-console.log("Server is running")
+console.log("Server is running" , server.port)
